ad49fefbec73a6698849e143d489a592e1886f77
Michal1898 <michal.volf@gmail.com>
2021-10-02 08:53:27 +0200

    Improved. Only 1 loop required.

--- main.py
+++ main.py
@@ -1,40 +1,14 @@
 def nested_squares(n):
-    for square_index in range(n - 1, -1, -1):
-        # print(square_index)
-        print(
-            (n - square_index - 1) * "# "
-            + (4 * square_index + 1) * "#"
-            + (n - square_index - 1) * " #"
-        )
-        if square_index:
-            print(
-                (n - square_index - 1) * "# "
-                + "#"
-                + (4 * square_index - 1) * " "
-                + "#"
-                + (n - square_index - 1) * " #"
-            )
-
-    for square_index in range(1, n):
-        # print(square_index)
-
-        if square_index:
-            print(
-                (n - square_index - 1) * "# "
-                + "#"
-                + (4 * square_index - 1) * " "
-                + "#"
-                + (n - square_index - 1) * " #"
-            )
-
-        print(
-            (n - square_index - 1) * "# "
-            + (4 * square_index + 1) * "#"
-            + (n - square_index - 1) * " #"
-        )
-
+    # create kernel (:-)
+    picture_of_squares = (n-1)*"# " + "#" + (n-1)*" #"+"\n"
+    for square_index in range(n-1 , 0, -1):
+        square_part1 =  (square_index)*"# "+(4* (n-square_index)-3 )*" "+(square_index)*" #"+"\n"
+        square_part2 = (square_index-1) * "# " + (4 * (n-square_index) +1) * "#" + (square_index-1) * " #" + "\n"
+        # Wrap kernel with next layers
+        picture_of_squares=square_part2+square_part1+picture_of_squares+square_part1+square_part2
+    print(picture_of_squares)
 
 if __name__ == "__main__":
-    for n in range (1,10):
-        print(f"n= {n}:")
+    for n in range (1,7):
+        print ("n=",n)
         nested_squares(n)
